ea6649d692cad0c73573bb926c7ad87537dbe209
Armando Eduardo <armandosoares414@gmail.com>
2025-03-16 20:25:15 -0300

    Add projeto completo
    projeto feito por completo, disposta a mudanças e melhoras.

---
+++ js/tarefas.js
@@ -0,0 +1,37 @@
+let input = document.getElementById('ipt-tarefa');
+let lista = document.getElementById('content');
+let count = 0;
+
+function adicionarTarefa(){
+    if(input.value.trim() === ""){
+        alert("informe uma tarefa válida");
+        return;
+    }
+    ++count;
+    let li = document.createElement("tasks");
+    li.innerHTML = `<div class="tasks" id="${count}">
+                    <div class="tarefa">${input.value}</div>
+                    <button onclick="concluirTarefa(${count})" id="tarefa-feita"><img src="assets/img/tarefa-concluida.png" alt="check-img"></button>
+                    <button onclick="deletarTarefa(${count})" id="tarefa-deletar"><img src="assets/img/lixeira.png" alt="lixeira-img"></button>
+                </div>`;
+    lista.appendChild(li);
+    input.value = "";
+}
+
+function concluirTarefa(count){
+    let item = document.getElementById(count);
+    item.classList.toggle("tarefa-concluida");
+    lista.appendChild(item);
+}
+
+input.addEventListener("keyup", function(event){
+    if(event.keyCode===13){
+        event.preventDefault();
+        Add.click();
+    }
+});
+
+function deletarTarefa(count){
+    let item = document.getElementById(count);
+    item.remove();
+}
